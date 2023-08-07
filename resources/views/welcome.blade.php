bb4a71863529692967aadab6e1cf383068b79d84
AcilRestu12 <arieskarestu214@gmail.com>
2023-08-07 08:20:05 +0700

    add section why and offer in home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
 	@vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body class="bg-background">
+	{{-- bg-blue-300 sm:bg-red-300 md:bg-green-300 lg:bg-yellow-300  --}}
+	
   	{{-- Navbar Start --}}
 	<nav class="bg-primary border-gray-200 dark:bg-primary">
 		<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -59,33 +61,91 @@
 						</svg>
 					</a>
 				</div>
-				<img class="h-auto md:max-w-md max-w-sm self-center justify-items-center" src="assets/book.png" alt="book illustration">
+				<img class="h-auto lg:max-w-md max-w-sm self-center justify-items-center" src="assets/book.png" alt="book illustration">
 			</div>
 		</div>
 	</section>
 	{{-- Jumbotron End --}}
 
 	{{-- Why start --}}
-	{{-- <section class="w-full">
+	<section class="w-full">
 		<h2 class="text-3xl md:text-4xl lg:text-5xl font-simonetta font-bold text-center mt-8 mb-6 md:mt-10 lg:mt-12 lg:mb-8">
 			Why Groove?
 		</h2>
-		<div class="flex flex-col md:px-6 bg-blue-300 sm:bg-red-300 md:bg-green-300 lg:bg-yellow-300 space-y-4 align-middle md:flex-row md:justify-center md:space-y-0">
-			<div class="mx-auto w-[60%] sm:w-[40%] md:w-1/4 md:h-[22.5rem] lg:h-[28rem] text-center bg-white border border-gray-200 rounded-lg shadow">
-				<img class="rounded-t-lg mx-auto max-w-xs md:w-[90%] lg:max-w-xs" src="assets/teacher.png" alt="teacher"/>
-				<h5 class="p-5 mb-2 md:mb-1 text-2xl lg:text-3xl font-bold tracking-tight text-gray-900">World-Class Experts</h5>
+		<div class="flex flex-col md:px-10 lg:px-24 space-y-4 align-middle md:flex-row md:justify-center md:space-y-0 lg:space-x-10">
+			<div class="mx-auto lg:mx-0 p-5 lg:p-3 mb-2 md:mb-0 w-1/2 sm:w-[40%] md:w-1/4 text-center bg-white border border-gray-200 rounded-lg shadow">
+				<img class="mx-auto w-2/3 lg:w-2/5" src="assets/teacher.png" alt="teacher"/>
+				<h5 class="text-xl pt-2 font-maitree font-bold tracking-tight text-gray-900 flex flex-col">
+					<span>World-Class</span>
+					<span>Experts</span>
+				</h5>
 			</div>
-			<div class="mx-auto w-[60%] sm:w-[40%] md:w-1/4 md:h-[22.5rem] lg:h-[28rem] text-center bg-white border border-gray-200 rounded-lg shadow">
-				<img class="rounded-t-lg mx-auto max-w-xs md:w-[90%] lg:max-w-xs" src="assets/budget.png" alt="budget"/>
-				<h5 class="p-5 mb-2 md:mb-1 text-2xl lg:text-3xl font-bold tracking-tight text-gray-900">Budget-Friendly Learning</h5>
+			<div class="mx-auto lg:mx-0 p-5 lg:p-3 mb-2 md:mb-0 w-1/2 sm:w-[40%] md:w-1/4 text-center bg-white border border-gray-200 rounded-lg shadow">
+				<img class="mx-auto w-2/3 lg:w-2/5" src="assets/budget.png" alt="budget"/>
+				<h5 class="text-xl pt-2 font-maitree font-bold tracking-tight text-gray-900 flex flex-col">
+					<span>Budget-Friendly</span>
+					<span>Learning</span>
+				</h5>
 			</div>
-			<div class="mx-auto w-[60%] sm:w-[40%] md:w-1/4 md:h-[22.5rem] lg:h-[28rem] text-center bg-white border border-gray-200 rounded-lg shadow">
-				<img class="rounded-t-lg mx-auto max-w-xs md:w-[90%] lg:max-w-xs" src="assets/sofa.png" alt="sofa"/>
-				<h5 class="p-5 mb-2 md:mb-1 text-2xl lg:text-3xl font-bold tracking-tight text-gray-900">Embrace Comfort, Embrace Tradition</h5>
+			<div class="mx-auto lg:mx-0 p-5 lg:p-3 mb-2 md:mb-0 w-1/2 sm:w-[40%] md:w-1/4 text-center bg-white border border-gray-200 rounded-lg shadow">
+				<img class="mx-auto w-2/3 lg:w-2/5" src="assets/sofa.png" alt="sofa"/>
+				<h5 class="text-xl pt-2 font-maitree font-bold tracking-tight text-gray-900 flex flex-col">
+					<span>Embrace Comfort, </span>
+					<span>Embrace Tradition </span>
+				</h5>
 			</div>
 		</div>
-	</section> --}}
+	</section>
 	{{-- Why End --}}
+
+	{{-- Offer Start --}}
+	<section class="bg-white">
+		<div class="container w-3/4 mx-auto mt-8 md:mt-10 lg:mt-12 py-4 sm:py-6 md:py-8 lg:py-12">
+			<h2 class="text-3xl md:text-4xl lg:text-5xl mb-6 sm:mb-7 font-simonetta font-bold text-center">
+				What We Offer
+			</h2>
+			<div class="w-full flex flex-col align-middle md:justify-center space-y-7 sm:space-y-8 md:space-y-9">
+				{{-- E-Learning --}}
+				<div class="w-full flex flex-col align-middle justify-evenly md:flex-row space-y-6 pb-5 sm:pb-6 md:pb-7">
+					<img class="h-auto w-2/3 md:w-2/5 lg:w-1/3 self-center" src="assets/video.png" alt="illustration video">
+					<div class="w-full md:w-1/2 px-5 sm:px-6 self-center">
+						<p class="text-sm lg:text-base font-montserrat font-bold text-slate-500 uppercase mb-2 lg:mb-3">E-Learning</p>
+						<h3 class="text-xl md:text-2xl lg:text-3xl font-maitree font-bold mb-1 md:mb-2 lg:mb-3">Educational Videos</h3>
+						<p class="text-base lg:text-lg font-maitree font-medium text-slate-800 mb-3 md:mb-4 lg:mb-5">
+							Engage with our carefully crafted videos that cover a range of topics, from basic phrases to advanced linguistic nuances.
+						</p>
+						<a href="#" class="block rounded-md font-montserrat font-semibold text-sm md:text-base w-fit py-1.5 px-5 md:px-6 lg:px-8 lg:py-2 text-center duration-200 text-primary hover:text-white hover:bg-primary border-primary border-2 focus:ring-4 focus:outline-none">See More</a>
+					</div>
+				</div>
+				{{-- BLog --}}
+				<div class="w-full flex flex-col align-middle justify-evenly md:flex-row-reverse space-y-6 pb-5 sm:pb-6 md:pb-7">
+					<img class="h-auto w-2/3 md:w-2/5 lg:w-1/3 self-center" src="assets/blog.png" alt="illustration blog">
+					<div class="w-full md:w-1/2 px-5 sm:px-6 self-center">
+						<p class="text-sm lg:text-base font-montserrat font-bold text-slate-500 uppercase mb-2 lg:mb-3">blog</p>
+						<h3 class="text-xl md:text-2xl lg:text-3xl font-maitree font-bold mb-1 md:mb-2 lg:mb-3">Comprehensive Resources</h3>
+						<p class="text-base lg:text-lg font-maitree font-medium text-slate-800 mb-3 md:mb-4 lg:mb-5">
+							Access a wealth of supplementary materials, exercises, and quizzes that complement your learning journey.						
+						</p>
+						<a href="#" class="block rounded-md font-montserrat font-semibold text-sm md:text-base w-fit py-1.5 px-5 md:px-6 lg:px-8 lg:py-2 text-center duration-200 text-primary hover:text-white hover:bg-primary border-primary border-2 focus:ring-4 focus:outline-none">See More</a>
+					</div>
+				</div>
+				{{-- Community --}}
+				<div class="w-full flex flex-col align-middle justify-evenly md:flex-row space-y-6 pb-5 sm:pb-6 md:pb-7">
+					<img class="h-auto w-2/3 md:w-2/5 lg:w-1/3 self-center" src="assets/community.png" alt="illustration community">
+					<div class="w-full md:w-1/2 px-5 sm:px-6 self-center">
+						<p class="text-sm lg:text-base font-montserrat font-bold text-slate-500 uppercase mb-2 lg:mb-3">community</p>
+						<h3 class="text-xl md:text-2xl lg:text-3xl font-maitree font-bold mb-1 md:mb-2 lg:mb-3">Community Support</h3>
+						<p class="text-base lg:text-lg font-maitree font-medium text-slate-800 mb-3 md:mb-4 lg:mb-5">
+							Join a vibrant community of language enthusiasts, share your progress, and learn from fellow Groove learners.
+						</p>
+						<a href="#" class="block rounded-md font-montserrat font-semibold text-sm md:text-base w-fit py-1.5 px-5 md:px-6 lg:px-8 lg:py-2 text-center duration-200 text-primary hover:text-white hover:bg-primary border-primary border-2 focus:ring-4 focus:outline-none">See More</a>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</section>
+	{{-- Offer End --}}
 
 	<div class="mb-80"></div>
 
